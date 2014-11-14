6fcf44b3fcf8bcf4eff69f0b96be98a558e56cb8
kfirL <landekfir@gmail.com>
2014-11-14 15:56:01 +0200

    working condition
    it worked on board of 100*100 and 4*5 threads for 20 iteration, it
    worked on all the lif files, if i think about any more optimization or i
    will update the project.
    
    we also need to add external documentation and some internal
    documentation.
    
    Ex1 file now also create P_output and P_output1 to compare into the the
    over output

--- CDP_HW1/src/ex1/ParallelGameOfLife.java
+++ CDP_HW1/src/ex1/ParallelGameOfLife.java
@@ -8,10 +8,9 @@ public class ParallelGameOfLife implements GameOfLife {
 
 	public boolean[][][] invoke(boolean[][] initalField, int hSplit,
 			int vSplit, int generations) {
-		boolean[][][] result = new boolean[2][][];
-		result[0] = invoke_aux(initalField, hSplit, vSplit, generations - 1);
-		result[1] = invoke_aux(result[0], hSplit, vSplit, 1);
-		return result;
+		
+		boolean [][][]tmp=invoke_aux(initalField, hSplit, vSplit, generations);		
+		return tmp;
 	}
 
 	/*
@@ -28,14 +27,14 @@ public class ParallelGameOfLife implements GameOfLife {
 		int num_row = 0;
 		double remaining_col = initalField.length;
 		double remaining_row = initalField[0].length;
-		for (int i = 0; i <= vSplit; i++) {
+		for (int i = 0; i < vSplit; i++) {
 			start_col = (int) (initalField.length - remaining_col);
-			num_col = (int) (Math.ceil(remaining_col) / (vSplit + 1 - i));
-			for (int j = 0; j <= hSplit; j++) {
+			num_col = (int) (Math.ceil(remaining_col) / (vSplit - i));
+			for (int j = 0; j < hSplit; j++) {
 				start_row = (int) (initalField[0].length - remaining_row);
-				num_row = (int) (Math.ceil(remaining_row) / (hSplit + 1 - j));
+				num_row = (int) (Math.ceil(remaining_row) / (hSplit - j));
 				SectionController tmp = new SectionController(start_row,
-						start_col, num_row, num_col, generations,initalField);
+						start_col, num_row, num_col, generations);
 				section_Controllers.add(tmp);
 				threads.add(new Thread(tmp));
 				remaining_row -= num_row;
@@ -44,16 +43,30 @@ public class ParallelGameOfLife implements GameOfLife {
 			remaining_row = initalField[0].length;
 		}
 	}
-
-	private boolean[][] invoke_aux(boolean[][] initalField, int hSplit,
+	
+	private boolean[][][] invoke_aux(boolean[][] initalField, int hSplit,
 			int vSplit, int generations) {
 
 		ArrayList<SectionController> section_Controllers = new ArrayList<SectionController>();
 		ArrayList<Thread> threads = new ArrayList<Thread>();
-		SectionController.input = initalField.clone();
+		SectionController.input[0] = initalField.clone();
+		SectionController.input[1] = new boolean[initalField.length][initalField[0].length];
 		assign_threads(initalField, hSplit, vSplit, generations,
 				section_Controllers, threads);
-		SectionController.neighbours = section_Controllers;
+		for (SectionController i : section_Controllers) {
+			for (SectionController j : section_Controllers) {
+				// if i and j are neighbours
+				if ((i.starting_row+i.num_of_rows==j.starting_row || 
+						j.starting_row+j.num_of_rows==i.starting_row || 
+						j.starting_row==i.starting_row) &&
+						(i.starting_col+i.num_of_cols==j.starting_col || 
+						j.starting_col+j.num_of_cols==i.starting_col || 
+						j.starting_col==i.starting_col)){
+					i.neighbours.add(j);
+				}
+				
+			}
+		}
 		for (Thread thread : threads) {
 			thread.start();
 		}
@@ -61,10 +74,17 @@ public class ParallelGameOfLife implements GameOfLife {
 			try {
 				thread.join();
 			} catch (InterruptedException e) {
-				// TODO handle this exception
+				throw (new SectionController.SynchronizationFailed("master thread can't join"));
 			}
+		}
+		try {
+			Ex1.printArray(SectionController.input[0], "P_output.txt");
+			Ex1.printArray(SectionController.input[1], "P_output1.txt");
+		} catch (Exception e) {
+			System.err.println("Failed to write the output to file");
+			e.printStackTrace();
+			System.exit(-1);
 		}
 		return SectionController.input.clone();
 	}
-
 }
--- CDP_HW1/src/ex1/SectionController.java
+++ CDP_HW1/src/ex1/SectionController.java
@@ -10,7 +10,15 @@ import java.util.ArrayList;
  * 
  */
 public class SectionController implements Runnable {
-	public class EndOfFunction extends RuntimeException {
+	public static class TileGenerationIsTooEarly extends Exception {
+		private static final long serialVersionUID = 1L;
+
+		public TileGenerationIsTooEarly(String message) {
+			super(message);
+		}
+	}
+
+	public static class EndOfFunction extends RuntimeException {
 		public EndOfFunction(String message) {
 			super(message);
 		}
@@ -19,7 +27,7 @@ public class SectionController implements Runnable {
 		private static final long serialVersionUID = 1L;
 	}
 
-	public class SynchronizationFailed extends RuntimeException {
+	public static class SynchronizationFailed extends RuntimeException {
 		public SynchronizationFailed(String message) {
 			super(message);
 		}
@@ -39,47 +47,54 @@ public class SectionController implements Runnable {
 			this.prev_gen_data = prev_gen_data;
 		}
 
-		public synchronized boolean GetData(int gen) {
+		public synchronized boolean getDataAux(int gen)
+				throws TileGenerationIsTooEarly {
 			if (gen == generation) {
 				return curr_gen_data;
 			} else if (gen == generation - 1) {
 				return prev_gen_data;
 			} else {
-				System.out.println("panic!!!");// TODO throw an exception
-				return false;// shouldn't reach this
+				throw new TileGenerationIsTooEarly(
+						((Integer) generation).toString());
+
 			}
 		}
 	}
 
-	private Integer input_gen;
-	private Integer result_gen;
 	public int target_gen;
 	Tile[][] result; // it's initialized as [cols][rows]
 	public int starting_row;
 	public int starting_col;
 	public int num_of_rows;
 	public int num_of_cols;
+	public int count;
+	public boolean wasNotified;
 	public boolean changed;// if current iteration changed generation
 	public boolean done;// all tiles are in the target generation.
 	public ArrayList<SectionController> neighbours; // includes yourself
-	//FIXME add a static board
-	
-	
+	/*
+	 * input[0] is the starting input matrix at the start of the run input[1] is
+	 * the last generation matrix at the end of the run input[0] is the
+	 * generation matrix before the last (generation-1)
+	 */
+	public static boolean[][][] input = new boolean[2][][];
+
 	public SectionController(int starting_row, int starting_col,
-			int num_of_rows, int num_of_cols, int target_gen, boolean[][] input) {
-		input_gen = 0;
-		result_gen = 0;
+			int num_of_rows, int num_of_cols, int target_gen) {
 		this.target_gen = target_gen;
 		this.starting_col = starting_col;
 		this.starting_row = starting_row;
 		this.num_of_cols = num_of_cols;
 		this.num_of_rows = num_of_rows;
 		this.result = new Tile[num_of_cols][num_of_rows];
+		this.neighbours = new ArrayList<SectionController>();
+		this.wasNotified = false;
 		for (int i = 0; i < num_of_cols; i++) {
 			for (int j = 0; j < num_of_rows; j++) {
 				// we initialize the tiles so the prev gen is -1 (meaningless),
 				// and the current gen is 0.
-				result[i][j] = new Tile(0, false, input[i + starting_col][j
+				result[i][j] = new Tile(0, input[0][i + starting_col][j
+						+ starting_row], input[0][i + starting_col][j
 						+ starting_row]);
 			}
 		}
@@ -87,7 +102,17 @@ public class SectionController implements Runnable {
 		done = (target_gen == 0);// finish immediately if the target generation
 									// is 0.
 	}
-
+	private void paintCell(int col,int row,int adj){
+			result[col][row].prev_gen_data = result[col][row].curr_gen_data;
+			// calculate the current generation of this tile
+			result[col][row].curr_gen_data = false;
+			if (adj == 3
+					|| (result[col][row].prev_gen_data && adj == 2)) {
+				result[col][row].curr_gen_data = true;
+			}
+			// update generation and changed.
+			result[col][row].generation += 1;
+	}
 	@Override
 	public void run() {
 		while (!done) {
@@ -95,54 +120,71 @@ public class SectionController implements Runnable {
 							// done.
 			changed = false;// it will be made true if any tile advanced a
 							// generation.
+			ArrayList<SectionController> notifyedNeighbors = new ArrayList<SectionController>();
 			// iterate on the section
 			for (int i = 0; i < num_of_cols; i++) {
 				for (int j = 0; j < num_of_rows; j++) {
 					if (result[i][j].generation < target_gen) {
 						done = false;
 						int adj = numNeighbors(i, j);
-						if (adj >= 0) {// adj will be negative if can't be
+						if (adj >= 0) {// adj will be -1 if can't be
 										// calculated
-							synchronized (result[i][j]) {
-								result[i][j].prev_gen_data = result[i][j].curr_gen_data;
-								// calculate the current generation of this tile
-								result[i][j].curr_gen_data = false;
-								if (adj == 3
-										|| (result[i][j].prev_gen_data && adj == 2)) {
-									result[i][j].curr_gen_data = true;
+							if (i==0||j==0||i==num_of_cols-1||j==num_of_rows-1){
+								synchronized (result[i][j]) {
+									paintCell(i, j, adj);
 								}
-								// update generation and changed.
-								result[i][j].generation += 1;
 							}
+							else paintCell(i, j, adj);
 							changed = true;
 							if (i == 0 || i == num_of_cols - 1 || j == 0
 									|| j == num_of_rows - 1) {
-								// TODO notifyAllRelatedNeighbors(i,j); (all
+								notifyAllRelatedNeighbors(i, j,
+										notifyedNeighbors);// (all
 								// neighbors related to this)
 							}
 						}
 					}
 				}
-				// if we can't calculate any of our tiles
-				if (!changed) {
-					synchronized (this) {
-						try{
+			}
+			// notify all neighbours
+			for (SectionController sectionController : notifyedNeighbors) {
+				synchronized (sectionController) {
+					sectionController.wasNotified = true;
+					sectionController.count++;
+					sectionController.notify();
+				}
+
+			}
+			// if we can't calculate any of our tiles
+			if (!changed && !done) {
+				synchronized (this) {
+					while (!wasNotified) {
+						try {
 							this.wait();
 						} catch (InterruptedException e) {
-							//FIXME
+							throw new SynchronizationFailed(
+									"there is no more work to do and the wait has failed");
 						}
+
 					}
+					wasNotified = false;
 				}
 			}
 		}
-		//TODO - write last generation and the one before it to the global input matrix.
+		for (int i = 0; i < result.length; i++) {
+			for (int j = 0; j < result[0].length; j++) {
+				input[0][i + starting_col][j + starting_row] = result[i][j].prev_gen_data;
+				input[1][i + starting_col][j + starting_row] = result[i][j].curr_gen_data;
+			}
+
+		}
+
 	}
-	
-	//returns null if index out of bounds.
-	//TODO
+
 	private SectionController convertPointToThread(int col, int row) {
 		// check if out of bounds
-		if (col > input.length || row > input[0].length) {
+		if (col >= input[0].length || row >= input[0][0].length || col < 0
+				|| row < 0) {
 			return null;
 		}
 		for (SectionController t : neighbours) {
@@ -152,38 +194,74 @@ public class SectionController implements Runnable {
 				return t;
 			}
 		}
+		System.out.println(col + " " + row);
 		throw new EndOfFunction(
 				"convertPointToThread didnt find the point a thread owner");
 	}
 
 	private int numNeighbors(int col, int row) {
-		int counter = (result[col][row].prev_gen_data ? -1 : 0);
-		
+		int counter = (result[col][row].curr_gen_data ? -1 : 0);
 		for (int i = col - 1; i <= col + 1; i++) {
 			for (int j = row - 1; j <= row + 1; j++) {
-				// need to check what section controller owns the square. null if it's outside the board limits.
-				SectionController neighborOwner = convertPointToThread(i+starting_col, j+starting_row);//convert by index on the board
-				if(neighborOwner == null) {
+				// need to check what section controller owns the square. null
+				// if it's outside the board limits.
+				SectionController neighborOwner = convertPointToThread(i
+						+ starting_col, j + starting_row);// convert by index on
+															// the board
+				if (neighborOwner == null) {
 					continue; // always false.
 				}
 				try {
-					if(neighborOwner.getTileData(i,j,result[i][j].generation-1)) {
+					if (neighborOwner.getTileData(i + starting_col, j
+							+ starting_row, result[col][row].generation,
+							neighborOwner)) {
 						counter++;
 					}
-				} catch (Exception e) {
-					//FIXME, also create an exception
+				} catch (TileGenerationIsTooEarly e) {
+					return -1;
 				}
 			}
 		}
 		return counter;
 	}
 
-	private boolean getTileData(int col, int row, int generation) {
-		// TODO Auto-generated method stub
-		return false;
+	// col and row are location on the "global" matrix
+	private boolean getTileData(int col, int row, int generation,
+			SectionController sender) throws TileGenerationIsTooEarly {
+		// the current sectionController own the Tile
+		if (sender.starting_col == this.starting_col
+				&& sender.starting_row == this.starting_row) {
+			return result[col - starting_col][row - starting_row]
+					.getDataAux(generation);
+		}
+		synchronized (result[col - starting_col][row - starting_row]) {
+			return result[col - starting_col][row - starting_row]
+					.getDataAux(generation);
+		}
 	}
 
-	public void notifyAllRelatedNeighbors(int col, int row) {
-		// TODO 
+	// calculate by the column and row which of the Neighbouring thread should
+	// be notified about the new data;
+	public void notifyAllRelatedNeighbors(int col, int row,
+			ArrayList<SectionController> notifyedNeighbors) {
+		int global_col = col + starting_col;
+		int global_row = row + starting_row;
+		for (SectionController tmp : neighbours) {
+			if (tmp == this)
+				continue;
+			// if tmp is next to the tile (col,row)
+
+			if (((global_row + 1 < tmp.starting_row + num_of_rows && global_row + 1 >= tmp.starting_row)
+					|| (global_row - 1 < tmp.starting_row + num_of_rows && global_row - 1 >= tmp.starting_row) || (global_row < tmp.starting_row
+					+ num_of_rows && global_row >= tmp.starting_row))
+					&& ((global_col + 1 < tmp.starting_col + num_of_cols && global_col + 1 >= tmp.starting_col)
+							|| (global_col - 1 < tmp.starting_col + num_of_cols && global_col - 1 >= tmp.starting_col) || (global_col < tmp.starting_col
+							+ num_of_cols && global_col >= tmp.starting_col)))
+
+				if (!notifyedNeighbors.contains(tmp)) {
+					notifyedNeighbors.add(tmp);
+				}
+
+		}
 	}
 }
