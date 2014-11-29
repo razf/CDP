7581342330136b3ee8916de2e2300cdf58f6d6f7
razf <rf6699@gmail.com>
2014-11-29 15:03:17 +0200

    Serial CT  algorithm
    implemented the serial version of the fast transformation. gets a
    segfault!

--- CDP_HW2/parallel-walsh.c
+++ CDP_HW2/parallel-walsh.c
@@ -10,6 +10,9 @@
 #define  GetHadamarTile(row,col) ({int Macro_tmp = row&col;\
 	Macro_tmp = NumberOfSetBits(Macro_tmp) & 1;\
 	 (1-2*Macro_tmp);})
+
+#define SWAP(a,b)({int* Macro_tmp=b; b=a; a=Macro_tmp;})
+
 	// Macro_tmp?-1:1______i checked and the mult is faster then branch
 /*int NumberOfSetBits(int i)
 {
@@ -25,8 +28,42 @@ int GetHadamarTile(int row,int col){
 	return 1-2*tmp;
 }*/
 
+
+
+void ct_phase(int* input, int* output, int vector_size, int working_size, int starting_index) {
+	register int upper_bound = working_size+starting_index;
+	for(register int i=starting_index; i<upper_bound; i++){
+		register int working_index = i+(vector_size>>1);
+		output[i]=input[i]+input[working_index];
+		output[working_index] = input[i]-input[working_index];
+	}
+}
+
 void fast_parallel_walsh(int* vector, int size)
 {
+	register int* temp_out_vector = malloc(sizeof(int) * size);
+	if(!temp_out_vector) {//failed to malloc
+		printf("Failed to allocate memory for input!");
+		exit(-1);
+	}
+	register int* temp_inp_vector = vector;
+	register int log_times = 0;
+	register int working_size;
+	for(register int i = size; i>1; i>>1){
+		log_times++;
+		working_size = i>>1;
+		for(register int j = 0; j<log_times; j++){
+			ct_phase(temp_inp_vector, temp_out_vector, size, working_size , j*working_size);
+			SWAP(temp_inp_vector, temp_out_vector);
+		}
+	}
+	if (vector==temp_out_vector){
+		free(temp_inp_vector);
+		return;
+	}
+	memcpy(vector,temp_out_vector , sizeof(int) * size);
+	free(temp_out_vector);
+
 }
 
 void simple_parallel_walsh(int* vector, int size)
@@ -42,9 +79,9 @@ void simple_parallel_walsh(int* vector, int size)
 	#pragma omp parallel
 	{
 		#pragma omp for schedule(static, 1 + ((size-1)/omp_get_num_threads()))
-		for(int col = 0; col<size; col++) {
+		for(register int col = 0; col<size; col++) {
 			register int sum = 0;
-			for(int row = 0; row<size; row++) {
+			for(register int row = 0; row<size; row++) {
 				sum+=GetHadamarTile(row,col)*input[row];
 			}
 			vector[col] = sum;
@@ -53,3 +90,4 @@ void simple_parallel_walsh(int* vector, int size)
 	}
 	free(input);
 }
+
